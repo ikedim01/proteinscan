ccb8c3efe4216149937c66cd07528503c44987dc
Isaac Dimitrovsky <ikedim@gmail.com>
2021-10-16 13:14:32 -0400

    finishing readuniprot

--- proteinscan/_nbdev.py
+++ proteinscan/_nbdev.py
@@ -7,7 +7,20 @@ index = {"openGzipOrText": "00_utils.ipynb",
          "datEntryLnsWithCode": "01_readuniprot.ipynb",
          "datEntryPrimaryAC": "01_readuniprot.ipynb",
          "scanDat": "01_readuniprot.ipynb",
-         "allPrimaryACsInDat": "01_readuniprot.ipynb"}
+         "allPrimaryACsInDat": "01_readuniprot.ipynb",
+         "datEntryPE": "01_readuniprot.ipynb",
+         "datEntryName": "01_readuniprot.ipynb",
+         "datEntrySeq": "01_readuniprot.ipynb",
+         "datEntryGOLines": "01_readuniprot.ipynb",
+         "datEntryGOTermPresent": "01_readuniprot.ipynb",
+         "datEntryIsAtpBinding": "01_readuniprot.ipynb",
+         "datEntryIsGtpBinding": "01_readuniprot.ipynb",
+         "datEntryIsMetalBinding": "01_readuniprot.ipynb",
+         "datEntryKWs": "01_readuniprot.ipynb",
+         "listIfStr": "01_readuniprot.ipynb",
+         "filterDatEntry": "01_readuniprot.ipynb",
+         "aaLetters": "01_readuniprot.ipynb",
+         "aaLettersSet": "01_readuniprot.ipynb"}
 
 modules = ["utils.py",
            "readuniprot.py"]
